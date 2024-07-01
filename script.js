120a6cf1b58aed8b33793bc92647fee46c8d7887
Eli tamang <elitamang024@gmail.com>
2024-07-01 15:15:52 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// var  bottle ="pani"
+// let bottle ='pani'
+// const bottle ='pani'
+const friend1 ='eli'
+const friend2 ='aryan'
+const friend3 ='ngela'
+
+
+
+const friends =['eli','aryan','ngela']
+
+const name ="Eli Tamang"
+const age =20
+const address = "dharan"
+
+const me = {
+ name : 'Eli Tamang',
+ age : 20,
+ address : 'dharan'
+}
